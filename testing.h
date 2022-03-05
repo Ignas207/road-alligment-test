0df9b1c39c81dd1626e9ad4c15a85d3faf882193
Ignas207 <61210167+Ignas207@users.noreply.github.com>
2022-03-05 16:38:11 +0200

    Printing works!

--- testing.h
+++ testing.h
@@ -4,9 +4,9 @@
 #include "road_alligment.h"
 #include <stdbool.h>
 
-#define X 120
-#define Y 160
+#define X 30
+#define Y 40
 
-void Generator(bool **image);
+void Generator(void *image);
 
 #endif
--- testing_thing.c
+++ testing_thing.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include "testing.h"
+
 int main(void)
 {
 
     bool image[X][Y] = {false};
 
-    Generator(&image);
+    Generator((void*)&image);
 
     return 0;
 }
 
 
-void Generator(bool **image)
+void Generator(void *image)
 {
-  //  bool **image2 = image;
+  bool **image2 = (bool*)&image;
     int i = 0;
     int j = 0;
     for(i = 0; i < X; i++)
     {
         for(j = 0; j < Y; j++)
         {
-            if(image[i][j] == true)
-                printf("1  ");
+            if(*((*image2 +i) +j) == true)
+                printf("1 ");
             else
-                printf("0  ");
+                printf("0 ");
         }
         printf("\n");
     }
