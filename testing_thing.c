354d69e4f1d5a49e2b303403b341e4d5320b89dd
Ignas207 <61210167+Ignas207@users.noreply.github.com>
2022-03-05 14:12:26 +0200

    Need to debug this

--- testing_thing.c
+++ testing_thing.c
@@ -13,5 +13,20 @@ int main(void)
 
 void Generator(bool **image)
 {
-    printf("This is working!\n");
+  //  bool **image2 = image;
+    int i = 0;
+    int j = 0;
+    for(i = 0; i < X; i++)
+    {
+        for(j = 0; j < Y; j++)
+        {
+            if(image[i][j] == true)
+                printf("1  ");
+            else
+                printf("0  ");
+        }
+        printf("\n");
+    }
+
+    //printf("i = %d\n", &i);
 }
